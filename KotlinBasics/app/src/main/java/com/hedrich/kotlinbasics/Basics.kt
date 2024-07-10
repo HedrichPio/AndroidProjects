15e221ec278a64b1f554b6b2e2b7b5e1b4ace1d1
HedrichPio <mhpiofernando1@gmail.com>
2024-07-11 00:43:00 +0200

    learning val,var,int,short,long,byte, float and double

---
+++ KotlinBasics/app/src/main/java/com/hedrich/kotlinbasics/Basics.kt
@@ -0,0 +1,14 @@
+package com.hedrich.kotlinbasics
+
+fun main(){
+    println("Hello World!")
+    val name= "Hedrich"
+    val myBirthYear : Short = 1999
+    var myAge : Byte = 25
+
+    var pi = 3.142f
+    var doublePi = 3.142
+
+    println("Hello $name")
+}
+
