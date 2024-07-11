c99a5bdc11300d5cf05d93832bf985d913800a01
HedrichPio <mhpiofernando1@gmail.com>
2024-07-11 23:49:31 +0200

    comparison and if statement

--- KotlinBasics/app/src/main/java/com/hedrich/kotlinbasics/Basics.kt
+++ KotlinBasics/app/src/main/java/com/hedrich/kotlinbasics/Basics.kt
@@ -5,10 +5,21 @@ fun main(){
     val name= "Hedrich"
     val myBirthYear : Short = 1999
     var myAge : Byte = 25
+    var day = 30
 
     var pi = 3.142f
     var doublePi = 3.142
 
     println("Hello $name")
+
+
+
+    val age = readln().toInt()
+
+    if(age>=18){
+        println("Entrance Allowed!")
+    }else{
+        println("Entrance Denied!")
+    }
 }
 
