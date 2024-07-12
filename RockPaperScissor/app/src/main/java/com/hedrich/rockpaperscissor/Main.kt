165cc52f3fd392abaf6f2763d581e5aed4a1254d
HedrichPio <mhpiofernando1@gmail.com>
2024-07-12 11:47:28 +0200

    added logic for rock paper scissor game

--- RockPaperScissor/app/src/main/java/com/hedrich/rockpaperscissor/Main.kt
+++ RockPaperScissor/app/src/main/java/com/hedrich/rockpaperscissor/Main.kt
@@ -1,5 +1,29 @@
 package com.hedrich.rockpaperscissor
 
 fun main(){
-  
+    println("Initiating Game Rock Paper Scissor...")
+    println("Please enter your choice :")
+
+    val playerChoice = readln().toString()
+
+    val randomNumber = (1..3).random()
+
+    var computerChoice = ""
+
+    when(randomNumber){
+        1->{computerChoice="rock"}
+        2->{computerChoice="paper"}
+        3->{computerChoice="scissor"}
+    }
+
+    val winner = when{
+        playerChoice==computerChoice-> "Tie"
+        playerChoice=="Rock" && computerChoice=="Scissor"-> "Player"
+        playerChoice=="Paper" && computerChoice=="Rock" -> "Player"
+        playerChoice=="Scissor" && computerChoice=="Paper"-> "Player"
+        else -> {"Computer"}
+    }
+
+    println("$winner Wins !!!")
+
 }
