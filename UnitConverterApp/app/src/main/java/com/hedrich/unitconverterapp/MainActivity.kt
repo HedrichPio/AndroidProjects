957f5f6c15bf0f5e048b851fbc446036a6fae607
HedrichPio <mhpiofernando1@gmail.com>
2024-07-18 01:06:44 +0200

    completed logic of the unit converter app

--- UnitConverterApp/app/src/main/java/com/hedrich/unitconverterapp/MainActivity.kt
+++ UnitConverterApp/app/src/main/java/com/hedrich/unitconverterapp/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.hedrich.unitconverterapp
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -24,12 +23,16 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hedrich.unitconverterapp.ui.theme.UnitConverterAppTheme
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,50 +53,125 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
 fun UnitConverter(modifier: Modifier = Modifier){
+
+    var inputValue by remember { mutableStateOf("") }
+    var outputValue by remember { mutableStateOf("") }
+
+    var dropOne by remember { mutableStateOf(false) }
+    var dropTwo by remember { mutableStateOf(false) }
+
+    var inMultiplier by remember { mutableStateOf(1.0) }
+    var outMultiplier by remember { mutableStateOf(1.0) }
+
+    var inputUnit by remember { mutableStateOf("m") }
+    var outputUnit by remember { mutableStateOf("m") }
+
+    var dropOneText by remember {mutableStateOf("Select") }
+    var dropTwoText by remember {mutableStateOf("Select") }
+
+    fun convert(){
+        val inputValueDouble = inputValue.toDoubleOrNull() ?: 0.0
+        val valueInMetres = (inputValueDouble * inMultiplier * 100.0).roundToInt() / 100.0
+        outputValue = (valueInMetres * outMultiplier).toString()
+    }
+
     Column(
         modifier=Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally) {
+
         Text(text = "Unit Converter")
         Spacer(modifier = Modifier.height(16.dp))
-        OutlinedTextField(value = "", onValueChange = {/*can enter a logic to perform a task*/})
+
+        OutlinedTextField(
+            value = inputValue,
+            onValueChange = {
+                inputValue=it
+                convert()},
+            label = {Text(text = "Enter Value")})
+
         Spacer(modifier = Modifier.height(16.dp))
+
         Row {
-//            val context = LocalContext.current
-//            Button(onClick = { Toast.makeText(context, "I am a toast...", Toast.LENGTH_SHORT).show() }) {
-//                Text(text = "Button One")
-//            }
+
             Box {
-                Button(onClick = { /*TODO*/ }) {
-                    Text(text = "Select")
-                Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")}
-                DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
-                    DropdownMenuItem(text = { Text(text = "Centimeters") }, onClick = { /*TODO*/ })
-                    DropdownMenuItem(text = { Text(text = "Meters") }, onClick = { /*TODO*/ })
-                    DropdownMenuItem(text = { Text(text = "Feet") }, onClick = { /*TODO*/ })
-                    DropdownMenuItem(text = { Text(text = "Milimeters") }, onClick = { /*TODO*/ })
+                Button(onClick = { dropOne=true }) {
+                    Text(text = dropOneText)
+                    Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")}
+
+                DropdownMenu(expanded = dropOne, onDismissRequest = { dropOne=false }) {
+                    DropdownMenuItem(text = { Text(text = "Centimeters") }, onClick = {
+                        dropOne=false
+                        dropOneText="cm"
+                        inputUnit="cm"
+                        inMultiplier=0.01
+                        convert() })
+                    DropdownMenuItem(text = { Text(text = "Meters") }, onClick = {
+                        dropOne=false
+                        dropOneText="m"
+                        inputUnit="m"
+                        inMultiplier=1.0
+                        convert()})
+                    DropdownMenuItem(text = { Text(text = "Feet") }, onClick = {
+                        dropOne=false
+                        dropOneText="ft"
+                        inputUnit="ft"
+                        inMultiplier=0.3048
+                        convert()})
+                    DropdownMenuItem(text = { Text(text = "Milimeters") }, onClick = {
+                        dropOne=false
+                        dropOneText="mm"
+                        inputUnit="mm"
+                        inMultiplier=0.001
+                        convert()})
                 }
             }
             Spacer(modifier = Modifier.width(16.dp))
+
             Box {
-                Button(onClick = { /*TODO*/ }) {
-                    Text(text = "Select")
+                Button(onClick = { dropTwo=true }) {
+                    Text(text = dropTwoText)
                     Icon(imageVector = Icons.Default.ArrowDropDown, contentDescription = "Arrow Down")}
-                DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
-                    DropdownMenuItem(text = { Text(text = "Centimeters") }, onClick = { /*TODO*/ })
-                    DropdownMenuItem(text = { Text(text = "Meters") }, onClick = { /*TODO*/ })
-                    DropdownMenuItem(text = { Text(text = "Feet") }, onClick = { /*TODO*/ })
-                    DropdownMenuItem(text = { Text(text = "Milimeters") }, onClick = { /*TODO*/ })
+
+                DropdownMenu(expanded = dropTwo, onDismissRequest = { dropTwo=false }) {
+                    DropdownMenuItem(text = { Text(text = "Centimeters") }, onClick = {
+                        dropTwo=false
+                        dropTwoText="cm"
+                        outputUnit="cm"
+                        outMultiplier=100.0
+                        convert()})
+                    DropdownMenuItem(text = { Text(text = "Meters") }, onClick = {
+                        dropTwo=false
+                        dropTwoText="m"
+                        outputUnit="m"
+                        outMultiplier=1.0
+                        convert()})
+                    DropdownMenuItem(text = { Text(text = "Feet") }, onClick = {
+                        dropTwo=false
+                        dropTwoText="ft"
+                        outputUnit="ft"
+                        outMultiplier=3.280839895
+                        convert()})
+                    DropdownMenuItem(text = { Text(text = "Milimeters") }, onClick = {
+                        dropTwo=false
+                        dropTwoText="mm"
+                        outputUnit="mm"
+                        outMultiplier=1000.0
+                        convert()})
                 }
             }
 
         }
         Spacer(modifier = Modifier.height(16.dp))
-        Text(text = "Result:")
+        Text(text = "Result: $outputValue $outputUnit")
     }
 }
+
+
+
 
 
 @Preview(showBackground = true)
