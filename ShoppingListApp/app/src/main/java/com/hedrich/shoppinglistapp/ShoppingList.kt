782e91f618b2dbc8dd57888a9af3ccbabf3a9f08
HedrichPio <mhpiofernando1@gmail.com>
2024-07-19 23:57:20 +0200

    furthur implementation of add items alert dialog UI and utilised it to add items and view the item list

--- ShoppingListApp/app/src/main/java/com/hedrich/shoppinglistapp/ShoppingList.kt
+++ ShoppingListApp/app/src/main/java/com/hedrich/shoppinglistapp/ShoppingList.kt
@@ -1,5 +1,7 @@
 package com.hedrich.shoppinglistapp
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,8 +10,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,6 +26,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 data class ShoppingItem(
@@ -42,7 +50,9 @@ fun ShoppingListApp(modifier: Modifier){
         LazyColumn(modifier = Modifier
             .fillMaxSize()
             .padding(16.dp)) {
-            items(sItems){}
+            items(sItems){
+                ShoppingListItem(it, { }, { })
+            }
         }
     }
 
@@ -53,10 +63,18 @@ fun ShoppingListApp(modifier: Modifier){
                 Row (modifier = Modifier
                     .fillMaxWidth()
                     .padding(8.dp), horizontalArrangement = Arrangement.SpaceEvenly){
-                    Button(onClick = { /*TODO*/ }) {
+                    Button(onClick = { showDialog=false }) {
                         Text(text = "Cancel")
                     }
-                    Button(onClick = { /*TODO*/ }) {
+                    Button(onClick = {
+                        if (itemName.isNotBlank()){
+                            val newItem = ShoppingItem(id = sItems.size + 1, name = itemName, quantity = itemQty.toInt())
+                            sItems = sItems + newItem
+                            showDialog=false
+                            itemName=""
+                            itemQty=""
+                        }
+                    }) {
                         Text(text = "Add")
                     }
                 }
@@ -68,15 +86,42 @@ fun ShoppingListApp(modifier: Modifier){
                         value = itemName,
                         onValueChange = {itemName=it},
                         singleLine = true,
-                        modifier = Modifier.fillMaxWidth().padding(8.dp),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp),
                         label = { Text(text = "Enter Item Name")})
                     OutlinedTextField(
                         value = itemQty,
                         onValueChange = {itemQty=it},
                         singleLine = true,
-                        modifier = Modifier.fillMaxWidth().padding(8.dp),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp),
                         label = { Text(text = "Enter Quantity")})
                 }
             })
     }
+}
+
+
+@Composable
+fun ShoppingListItem(item: ShoppingItem, onEditClick: () -> Unit, onDeleteClick: () -> Unit){
+
+    Row (
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .border(border = BorderStroke(2.dp, Color.Black), shape = RoundedCornerShape(20)),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ){
+        Text(text = item.name)
+        Text(text = "${item.quantity}")
+        Button(onClick = { onEditClick() }) {
+            Icon(imageVector = Icons.Default.Edit, contentDescription = "Edit")
+        }
+        Button(onClick = { onDeleteClick() }) {
+            Icon(imageVector = Icons.Default.Delete, contentDescription = "Delete")
+        }
+    }
+
 }
