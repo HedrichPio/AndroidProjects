b96355df296ae6c809c2e253903825bb9b04df08
HedrichPio <mhpiofernando1@gmail.com>
2024-08-08 22:13:59 +0200

    Added Navigation App

---
+++ NavigationApp/app/src/main/java/com/hedrich/navigationapp/MainActivity.kt
@@ -0,0 +1,103 @@
+package com.hedrich.navigationapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.hedrich.navigationapp.ui.theme.NavigationAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NavigationAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background) {
+
+                    MyApp()
+
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun MyApp(){
+    val navController = rememberNavController()
+    
+    NavHost(navController = navController, startDestination = "firstScreen") {
+        composable("firstScreen"){
+            FirstScreen ({ navController.navigate("secondScreen")})
+        }
+        composable("secondScreen"){
+            SecondScreen({navController.navigate("firstScreen")})
+        }
+    }
+}
+
+
+
+@Composable
+fun FirstScreen(navigateToSecondScreen:()->Unit){
+
+    val name = remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+
+        Text(text = "Welcome", fontSize = 24.sp)
+        Spacer(modifier = Modifier.height(16.dp))
+        OutlinedTextField(value = name.value, onValueChange = { name.value=it } )
+        Button(onClick = { navigateToSecondScreen() }) {
+            Text(text = "Go to Second Screen")
+        }
+    }
+}
+
+@Composable
+fun SecondScreen(navigateToFirstScreen:()->Unit){
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+
+        Text(text = "Hi you have navigated to the Second Screen", fontSize = 24.sp)
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = { navigateToFirstScreen() }) {
+            Text(text = "Go to First Screen")
+        }
+    }
+}
