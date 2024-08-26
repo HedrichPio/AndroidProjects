fe8a3819a8bb4cf048e2c12a352c19c3477eccd1
HedrichPio <mhpiofernando1@gmail.com>
2024-08-26 23:27:34 +0200

    Wishlist app - Added Floating action button to main screen

--- WishListApp/app/src/main/java/com/hedrich/wishlistapp/HomeView.kt
+++ WishListApp/app/src/main/java/com/hedrich/wishlistapp/HomeView.kt
@@ -4,23 +4,41 @@ import android.widget.Toast
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeView(modifier:Modifier){
     val context = LocalContext.current
     Scaffold(
+        modifier=modifier,
         topBar = {
             AppBarView(
                 title = "Wishlist",
                 { Toast.makeText(context,"Button Clicked", Toast.LENGTH_LONG).show() }) },
 
-        modifier=modifier) { innerPadding->
+        floatingActionButton = {
+            FloatingActionButton(
+                modifier = Modifier.padding(20.dp),
+                contentColor = Color.White,
+                backgroundColor = Color.Black,
+                onClick = { Toast.makeText(context,"Plus Button Clicked", Toast.LENGTH_LONG).show() })  {
 
-        LazyColumn(modifier = Modifier.fillMaxSize().padding(innerPadding)) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = null)
+            }
+        }) { innerPadding->
+
+        LazyColumn(modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPadding)) {
 
         }
     }
