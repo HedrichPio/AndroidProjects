bba12308b112f247566fd8c3c3dbd623be63793f
HedrichPio <mhpiofernando1@gmail.com>
2024-08-30 00:08:53 +0200

    Wishlist app - setting up wish database and DAO

--- WishListApp/app/src/main/java/com/hedrich/wishlistapp/data/WishDao.kt
+++ WishListApp/app/src/main/java/com/hedrich/wishlistapp/data/WishDao.kt
@@ -1,9 +1,11 @@
 package com.hedrich.wishlistapp.data
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -14,5 +16,14 @@ abstract class WishDao {
 
     @Query("Select * from `wish-table`")
     abstract fun getAllWishes(): Flow<List<Wish>>
+
+    @Update
+    abstract suspend fun updateWish(wishEntity: Wish)
+
+    @Delete
+    abstract suspend fun deleteWish(wishEntity: Wish)
+
+    @Query("Select * from `wish-table` where id=:id")
+    abstract fun getWishById(id:Long): Flow<Wish>
 
 }
