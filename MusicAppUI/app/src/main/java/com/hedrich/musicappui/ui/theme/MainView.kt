b0d12f5d971b73023cfc1f1309f1e7af0e16735c
HedrichPio <mhpiofernando1@gmail.com>
2024-09-09 23:48:08 +0200

    Music App UI - Started implementing the main UI, created screens class and screen objects, created Drawer Item to be used in a drawer menu

--- MusicAppUI/app/src/main/java/com/hedrich/musicappui/ui/theme/MainView.kt
+++ MusicAppUI/app/src/main/java/com/hedrich/musicappui/ui/theme/MainView.kt
@@ -1,16 +1,58 @@
 package com.hedrich.musicappui.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.hedrich.musicappui.Screen
+import com.hedrich.musicappui.screensInDrawer
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainView(modifier: Modifier){
 
-    Scaffold() { innerPadding ->
+    val scaffoldState:ScaffoldState = rememberScaffoldState()
+    val scope:CoroutineScope = rememberCoroutineScope()
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Home") },
+                navigationIcon = {
+                    IconButton(
+                        onClick = { scope.launch { scaffoldState.drawerState.open() } }) {
+                        Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "Menu")
+                    }
+                }
+            )
+        },
+        scaffoldState=scaffoldState,
+        drawerContent = {
+
+        }
+    ) { innerPadding ->
         Column(modifier = Modifier.padding(innerPadding)) {
             Text(text = "Hello")
         }
@@ -18,3 +60,24 @@ fun MainView(modifier: Modifier){
 }
 
 
+@Composable
+fun DrawerItem(selected:Boolean,item:Screen.DrawerScreen,onDrawerItemClicked:()-> Unit){
+
+    val background = if(selected) Color.DarkGray else Color.White
+
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp, vertical = 16.dp)
+            .background(background)
+            .clickable { onDrawerItemClicked() }) {
+
+        Icon(
+            painter = painterResource(id = item.icon),
+            contentDescription = item.dTitle,
+            Modifier.padding(end = 8.dp, top = 4.dp))
+
+        Text(text = item.dTitle, style = MaterialTheme.typography.labelMedium)
+    }
+
+}
